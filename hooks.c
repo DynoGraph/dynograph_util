5bf7139231bd3890b9710b74113ab2d714feb806
Eric Hein <ehein6@gatech.edu>
2017-04-11 16:20:15 -0400

    Add starttiming/stoptiming

--- hooks.c
+++ hooks.c
@@ -1,7 +1,22 @@
 #include "hooks_c.h"
 #include <stdio.h>
-void hooks_region_begin(const char* name) {}
-void hooks_region_end() {}
+
+#ifdef __le64__
+#include <memoryweb.h>
+#endif
+
+void hooks_region_begin(const char* name)
+{
+#ifdef __le64__
+starttiming();
+#endif
+}
+void hooks_region_end()
+{
+#ifdef __le64__
+stoptiming();
+#endif
+}
 void hooks_set_attr_u64(const char * key, uint64_t value) {}
 void hooks_set_attr_i64(const char * key, int64_t value) { fprintf(stderr, "%s = %lli\n", key, value); }
 void hooks_set_attr_f64(const char * key, double value) {}
